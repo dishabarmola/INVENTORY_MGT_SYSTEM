045fae5dcc3fa6cd09f1fb973752ebcb63d04211
dishabarmola <120310851+dishabarmola@users.noreply.github.com>
2023-12-28 10:17:39 +0530

    Update update_product.py

--- update_product.py
+++ update_product.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 import mysql.connector
+import os
 
-# Create a connection to the MySQL database
+
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
@@ -9,35 +10,36 @@ mydb = mysql.connector.connect(
   database="collegedb"
 )
 
-# Create a cursor object to execute SQL queries
+
 mycursor = mydb.cursor()
 
-# Define a function to update a product in the database
+
+def redirect_to_homepage():
+    os.system("python homepage.py")
+
+
 def update_product():
-    # Get the product name, price, and stocks from the user input
+   
     product = product_entry.get()
     price = price_entry.get()
     stock = stock_entry.get()
     
-    # Define the SQL query to update the product
+   
     sql = "UPDATE stocks SET price = %s, stock = %s WHERE product = %s"
     values = (price, stock, product)
-    
-    # Execute the SQL query
+ 
     mycursor.execute(sql, values)
-    
-    # Commit the changes to the database
+
     mydb.commit()
-    
-    # Print a message to the user indicating that the update was successful
+
     message_label.config(text="Product updated successfully!")
+    
 
-# Create a tkinter window
 window = tk.Tk()
-window.geometry=("800x900")
-window["bg"]="black"
+window.geometry=("900x400")
 
-# Add some labels and entry boxes for the user input
+
+
 product_label = tk.Label(window, text="Product name:")
 product_label.pack()
 product_entry = tk.Entry(window)
@@ -53,13 +55,12 @@ stock_label.pack()
 stock_entry = tk.Entry(window)
 stock_entry.pack()
 
-# Add a button to submit the user input and update the product in the database
+
 update_button = tk.Button(window, text="Update Product", command=update_product)
 update_button.pack()
 
-# Add a label to display messages to the user
 message_label = tk.Label(window, text="")
 message_label.pack()
 
-# Start the tkinter mainloop
+
 window.mainloop()
