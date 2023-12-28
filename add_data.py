09604892c5e5bb564c71417c5ec32af5bfc8e077
dishabarmola <120310851+dishabarmola@users.noreply.github.com>
2023-12-28 10:00:22 +0530

    Update add_data.py

--- add_data.py
+++ add_data.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import mysql.connector
 
-# Connect to the database
+
 db = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -9,10 +9,10 @@ db = mysql.connector.connect(
     database="collegedb"
 )
 
-# Create a cursor to execute SQL statements
+
 cursor = db.cursor()
 
-# Create a function to insert data into the table
+
 def insert_data():
     
     username = username_entry.get()
@@ -20,27 +20,25 @@ def insert_data():
     contact = contact_entry.get()
     branch = branch_entry.get()
     
-    # Execute the SQL statement to insert the data
+
     sql = "INSERT INTO studnt (username, password, contact, branch) VALUES ( %s, %s, %s, %s)"
     values = (username, password, contact, branch)
     cursor.execute(sql, values)
     db.commit()
     
-    # Clear the entry fields
+  
     
     username_entry.delete(0, tk.END)
     password_entry.delete(0, tk.END)
     contact_entry.delete(0, tk.END)
     branch_entry.delete(0, tk.END)
-    
-    # Show a message to indicate the data was successfully inserted
+ 
     message_label.config(text="Data successfully inserted!")
 
-# Create the Tkinter window
 window = tk.Tk()
 window.title("Insert data into SQL table")
 
-# Create the entry fields for the user to input data
+
 
 
 username_label = tk.Label(window, text="Username:")
@@ -63,13 +61,13 @@ branch_label.grid(row=4, column=0)
 branch_entry = tk.Entry(window)
 branch_entry.grid(row=4, column=1)
 
-# Create a button to insert the data
+
 insert_button = tk.Button(window, text="Insert data", command=insert_data)
 insert_button.grid(row=5, column=0)
 
-# Create a label to display messages
+
 message_label = tk.Label(window, text="")
 message_label.grid(row=6, column=0)
 
-# Run the Tkinter main loop
+
 window.mainloop()
