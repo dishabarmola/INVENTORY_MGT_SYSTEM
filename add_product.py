d2b3d0e9d7a879fde61c8de63578d6606e96186f
dishabarmola <120310851+dishabarmola@users.noreply.github.com>
2023-12-28 10:03:13 +0530

    Update add_product.py

--- add_product.py
+++ add_product.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 import mysql.connector
+import os
 
-# Connect to the database
+
 db = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -9,38 +10,40 @@ db = mysql.connector.connect(
     database="collegedb"
 )
 
-# Create a cursor to execute SQL statements
 cursor = db.cursor()
 
-# Create a function to insert data into the table
+
+def redirect_to_homepage():
+    os.system("python homepage.py")
+
 def insert_data():
     
     product = product_entry.get()
     price = price_entry.get()
     stock = stock_entry.get()
   
-    
-    # Execute the SQL statement to insert the data
+   
     sql = "INSERT INTO stocks (product, price, stock) VALUES ( %s, %s, %s)"
     values = (product, price, stock)
     cursor.execute(sql, values)
     db.commit()
     
-    # Clear the entry fields
+
     
     product_entry.delete(0, tk.END)
     price_entry.delete(0, tk.END)
     stock_entry.delete(0, tk.END)
    
     
-    # Show a message to indicate the data was successfully inserted
-    message_label.config(text="Data successfully inserted!")
 
-# Create the Tkinter window
+    message_label.config(text="Data successfully inserted!")
+    
+
+
 window = tk.Tk()
 window.title("Insert data into SQL table")
+window.geometry("300x150")
 
-# Create the entry fields for the user to input data
 
 
 product_label = tk.Label(window, text="Product")
@@ -60,13 +63,11 @@ stock_entry.grid(row=3, column=1)
 
 
 
-# Create a button to insert the data
+
 insert_button = tk.Button(window, text="Insert data", command=insert_data)
 insert_button.grid(row=5, column=0)
 
-# Create a label to display messages
 message_label = tk.Label(window, text="")
 message_label.grid(row=6, column=0)
 
-# Run the Tkinter main loop
 window.mainloop()
